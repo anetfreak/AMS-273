d820df2e48674afa2153bad9b0cc7fa8e38b3eac
kanikaanand <online.kanika@gmail.com>
2013-12-07 20:56:10 -0800

    added

---
+++ src/com/dao/JMSClient.java
@@ -0,0 +1,28 @@
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+
+public class JMSClient implements MessageListener{
+
+	public static void main(String[] args) {
+	
+		
+
+	}
+
+	public void onMessage(Message message) {
+		TextMessage TM = (TextMessage)message;
+		try{
+		
+		System.out.println(TM.getText());
+		}
+		catch(JMSException j)
+		{
+			j.printStackTrace();
+		}
+		
+	}
+
+}
